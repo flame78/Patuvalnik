1fae66b263632dc12a3ddc3113d3c56f4576afce
flame78 <flame78@gmail.com>
2015-12-17 12:41:04 +0200

    add BaseViewModel and TrirpsViewModel

---
+++ Patuvalnik/Patuvalnik.Shared/ViewModels/BaseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Patuvalnik.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ Patuvalnik/Patuvalnik.Shared/ViewModels/TripsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Patuvalnik.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Patuvalnik.ViewModels
+{
+    public class TripsViewModel : BaseViewModel
+    {
+        private List<Trip> trips;
+
+        public List<Trip> Trips {  get
+            {
+                return this.trips;
+            }
+            set
+            {
+                if (this.trips == value) return;
+                this.trips = value;
+                NotifyPropertyChanged("Trips");
+            }
+        }
+    }
+}
