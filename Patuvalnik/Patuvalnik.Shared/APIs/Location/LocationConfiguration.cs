471b0ede93db57ea2db554bfd0c705b2fddae28b
MariaKaramatova <maria.karamatova@gmail.com>
2015-12-19 15:05:59 +0200

    location

---
+++ Patuvalnik/Patuvalnik.Shared/APIs/Location/LocationConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+
+namespace Patuvalnik.APIs.Location
+{
+    public partial class MainPage : Page
+    {
+        public const string FEATURE_NAME = "Geolocation";
+
+        List<Scenario> scenarios = new List<Scenario>
+        {
+            new Scenario() { Title = "Track position", ClassType = typeof(Patuvalnik.APIs.Location.Scenario1) },
+            new Scenario() { Title = "Get position", ClassType = typeof(Patuvalnik.APIs.Location.Scenario2) },
+        };
+    }
+
+    public class Scenario
+    {
+        public string Title { get; set; }
+
+        public Type ClassType { get; set; }
+
+        public override string ToString()
+        {
+            return Title;
+        }
+    }
+}
--- Patuvalnik/Patuvalnik.Shared/APIs/Location/Scenario2.xaml.cs
+++ Patuvalnik/Patuvalnik.Shared/APIs/Location/Scenario2.xaml.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -17,30 +15,16 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace Patuvalnik
+namespace Patuvalnik.APIs.Location
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Scenario2 : Page
     {
-        private Geolocator geolocator;
-
-        public MainPage()
+        public Scenario2()
         {
             this.InitializeComponent();
-            
-            this.geolocator = new Geolocator();
-            this.geolocator.PositionChanged += OnGeolocationPositionchanged;
-        }
-
-        private void OnGeolocationPositionchanged(Geolocator sender, PositionChangedEventArgs args)
-        {            
-            Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-            {
-                var lat = args.Position.Coordinate.Latitude;
-                var lon = args.Position.Coordinate.Longitude;
-            });
         }
     }
 }
